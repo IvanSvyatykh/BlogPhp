2150a1b3a10dc9671403104989b6ae9e40d16177
Ivan Svyatykh <zlativansvyat@mail.ru>
2025-05-06 16:32:35 +0500

    Initial commit

---
+++ web/index.php
@@ -0,0 +1,19 @@
+<?php
+
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $data = array('name' => 'Max', 'role' => 'web developer');
+    $response->getBody()->write(json_encode($data));
+
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->run();
