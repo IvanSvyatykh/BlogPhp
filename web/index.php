fdda1089f0924f598378643bde83b8ae6ffb8505
Ivan Svyatykh <zlativansvyat@mail.ru>
2025-05-08 19:14:33 +0500

    сделал запуск через docker

--- web/index.php
+++ web/index.php
@@ -6,7 +6,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 
-
+require dirname(__DIR__) . '/vendor/autoload.php';
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response, $args) {
