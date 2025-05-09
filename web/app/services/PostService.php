4dda0e53a64e97985089a0d78a1387b3ef0100c7
FazylovV <vladislav.fazylov04@gmail.com>
2025-05-09 16:29:03 +0500

    added post repository

---
+++ web/app/services/PostService.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace app\services;
+
+use app\entities\Post;
+use app\repositories\PostRepository;
+
+class PostService
+{
+    public function __construct(
+        private PostRepository $postRepository
+    ) {}
+
+    public function createPost(array $data, int $authorId): Post
+    {
+
+        $post = new Post(
+            $data['title'],
+            $data['content'],
+            $authorId
+        );
+
+        $this->postRepository->save($post);
+        return $post;
+    }
+
+    public function getPost(int $id): ?Post
+    {
+        return $this->postRepository->find($id);
+    }
+
+    public function getAllPosts(int $limit = 10, int $offset = 0): array
+    {
+        return $this->postRepository->findAll($limit, $offset);
+    }
+}
